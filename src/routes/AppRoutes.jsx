48e8121f9d83a6a71b34169cb132f5833db81c6e
Im-orbitt <clevernihal22@gmail.com>
2026-08-02 12:44:16 +0530

    feat: add application layouts

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,29 +1,42 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "../pages/LandingPage";
-import ParentPage from "../pages/ParentPage";
-import ProgressPage from "../pages/ProgressPage";
-import LessonPage from "../pages/LessonPage";
-import ChapterPage from "../pages/ChapterPage";
-import SubjectPage from "../pages/SubjectPage";
-import LibraryPage from "../pages/LibraryPage";
-import SettingsPage from "../pages/SettingsPage";
-import DashboardPage from "../pages/DashboardPage";
-import LoginPage from "../pages/LoginPage";
+
+import MainLayout from "../layouts/MainLayout";
+import AuthLayout from "../layouts/AuthLayout";
+import DashboardLayout from "../layouts/DashboardLayout";
+
+import Landing from "../pages/Landing";
+import Parent from "../pages/Parent";
+import Progress from "../pages/Progress";
+import Lesson from "../pages/Lesson";
+import Chapter from "../pages/Chapter";
+import Subject from "../pages/Subject";
+import Library from "../pages/Library";
+import Settings from "../pages/Settings";
+import Dashboard from "../pages/Dashboard";
+import Login from "../pages/Login";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/parent" element={<ParentPage />} />
-        <Route path="/progress" element={<ProgressPage />} />
-        <Route path="/lesson" element={<LessonPage />} />
-        <Route path="/chapter" element={<ChapterPage />} />
-        <Route path="/subject" element={<SubjectPage />} />
-        <Route path="/library" element={<LibraryPage />} />
-        <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Landing />} />
+          <Route path="parent" element={<Parent />} />
+          <Route path="progress" element={<Progress />} />
+          <Route path="lesson" element={<Lesson />} />
+          <Route path="chapter" element={<Chapter />} />
+          <Route path="subject" element={<Subject />} />
+          <Route path="library" element={<Library />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+
+        <Route path="/dashboard" element={<DashboardLayout />}>
+          <Route index element={<Dashboard />} />
+        </Route>
+
+        <Route path="/login" element={<AuthLayout />}>
+          <Route index element={<Login />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
