960eedf4d2a892d5576668a7599abab03be4639a
Im-orbitt <clevernihal22@gmail.com>
2026-08-02 18:09:59 +0530

    feat: scaffold landing page feature

---
+++ src/features/landing/CTA/CTA.jsx
@@ -0,0 +1,5 @@
+function CTA() {
+  return <h1>CTA</h1>;
+}
+
+export default CTA;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,17 @@
-function LandingPage() {
-  return <h1>Landing Page</h1>;
+import Hero from "../features/landing/Hero/Hero";
+import Features from "../features/landing/Features/Features";
+import CTA from "../features/landing/CTA/CTA";
+import FAQ from "../features/landing/FAQ/FAQ";
+
+function Landing() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <CTA />
+      <FAQ />
+    </>
+  );
 }
 
-export default LandingPage;
+export default Landing;
