8795cad72630b013ef438b13c660fc8370e9d590
Im-orbitt <clevernihal22@gmail.com>
2026-08-06 20:30:57 +0530

    refactor(layout): scaffold dashboard shell

--- src/components/layout/Sidebar/Sidebar.jsx
+++ src/components/layout/Sidebar/Sidebar.jsx
@@ -1,5 +1,23 @@
+import { NavLink } from "react-router-dom";
+
 function Sidebar() {
-  return <aside>Sidebar</aside>;
+  return (
+    <aside>
+      <h2>LearnPilot</h2>
+
+      <nav>
+        <NavLink to="/app/dashboard">Dashboard</NavLink>
+        <br />
+        <NavLink to="/app/library">Library</NavLink>
+        <br />
+        <NavLink to="/app/progress">Progress</NavLink>
+        <br />
+        <NavLink to="/app/parent">Parent</NavLink>
+        <br />
+        <NavLink to="/app/settings">Settings</NavLink>
+      </nav>
+    </aside>
+  );
 }
 
 export default Sidebar;
--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,5 +1,24 @@
 import { Outlet } from "react-router-dom";
 
-export default function DashboardLayout() {
-  return <Outlet />;
+import Sidebar from "../components/layout/Sidebar/Sidebar";
+import Topbar from "../components/layout/Topbar/Topbar";
+
+import "./DashboardLayout.css";
+
+function DashboardLayout() {
+  return (
+    <div className="dashboard-layout">
+      <Sidebar />
+
+      <div className="dashboard-content">
+        <Topbar />
+
+        <main className="dashboard-main">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
 }
+
+export default DashboardLayout;
