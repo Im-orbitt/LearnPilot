0b610b89f73188d59e0994f49398e74df0413c97
Im-orbitt <clevernihal22@gmail.com>
2026-08-17 20:07:05 +0530

    fix(web): mount Vercel Analytics in App

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 import App from "./App.jsx";
 
@@ -14,6 +13,5 @@ createRoot(document.getElementById("root")).render(
         <App />
       </BookProvider>
     </AuthProvider>
-    <Analytics />
   </StrictMode>,
 );
